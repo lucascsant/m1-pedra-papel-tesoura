b359f3c4851152c5c423a58391c58636e7251bff
lucassant.os <lucao48@gmail.com>
2025-08-27 15:05:31 -0300

    Pedra Papel e Tesoura

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    let resultado = "Empate!"
+    if(player1 == "Pedra" && player2 == "Pedra"){
+        resultado = "Empate!";
+        return resultado;
+    } else if (player1 == "Pedra" && player2 == "Tesoura"){
+        resultado = "Jogador 1 venceu!";
+        return resultado;
+    } else if (player1 == "Pedra" && player2 == "Papel"){
+        resultado = "Jogador 2 venceu!"
+        return resultado
+    } else if (player1 == "Papel" && player2 == "Pedra"){
+        resultado = "Jogador 1 venceu!"
+        return resultado
+    } else if (player1 == "Papel" && player2 == "Tesoura"){
+        resultado = "Jogador 2 venceu!"
+        return resultado
+    } else if (player1 == "Papel" && player2 == "Papel"){
+        resultado = "Empate!"
+        return resultado
+    } else if (player1 == "Tesoura" && player2 == "Pedra"){
+        resultado = "Jogador 2 venceu!"
+        return resultado
+    } else if (player1 == "Tesoura" && player2 == "Tesoura"){
+        resultado = "Empate!"
+        return resultado
+    } else if (player1 == "Tesoura" && player2 == "Papel"){
+        resultado = "Jogador 1 venceu!"
+        return resultado
+    }
+}
